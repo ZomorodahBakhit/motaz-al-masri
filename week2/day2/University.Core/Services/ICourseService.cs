a70ceeb9cddbf34655773d32f4660d3b82eef772
MotazAlMasri <gzdzfj9gxb@privaterelay.appleid.com>
2026-07-01 13:10:02 +0300

    Add files via upload

---
+++ week2/day2/University.Core/Services/ICourseService.cs
@@ -0,0 +1,15 @@
+﻿using University.Core.DTOs;
+using University.Core.Forms;
+
+
+namespace University.Core.Services
+{
+    public interface ICourseService
+    {
+        CourseDto GetById(int id);
+        void Create(CreateCourseForm form);
+        void Delete(int id);
+    }
+
+    
+}
---
+++ week2/day2/University.Data/Migrations/20260630170143_add-unique-constrain-to-the-student.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace University.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class adduniqueconstraintothestudent : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Students_Email",
+                table: "Students",
+                column: "Email",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Students_Email",
+                table: "Students");
+        }
+    }
+}
